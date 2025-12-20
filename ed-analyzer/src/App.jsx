663bb2a5b42cb484327dc0f9f6d995b693febb30
Akhil-Agarwal-123 <akhilagarwal79@gmail.com>
2025-12-19 19:55:27 -0800

    Slight accent width modification

--- ed-analyzer/src/App.jsx
+++ ed-analyzer/src/App.jsx
@@ -398,7 +398,7 @@ const AccordionPost = ({ post }) => {
     >
       {/* Decorative accent strip */}
       <div
-        className="absolute left-0 top-0 bottom-0 w-2 overflow-hidden"
+        className="absolute left-0 top-0 bottom-0 w-1.5 overflow-hidden"
         style={{
             backgroundColor: primaryColor
         }}
